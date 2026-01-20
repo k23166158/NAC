12090ba1d66ad0e73ccaef4652bc148d05a7c5b3
k23166158 <k23166158>
2026-01-20 11:57:00 +0000

    name update + file restructure

--- resolveme/asgi.py
+++ resolveme/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for largeGroupProjectNAC project.
+ASGI config for resolveme project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'largeGroupProjectNAC.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resolveme.settings')
 
 application = get_asgi_application()
