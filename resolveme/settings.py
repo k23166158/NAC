207193a60335ca2133fc672523f895fc6a64f212
Akshaye Sharma <akshaye.bhatt@gmail.com>
2026-01-21 10:37:45 +0000

    Update user model

--- resolveme/settings.py
+++ resolveme/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+AUTH_USER_MODEL = 'tickets.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
