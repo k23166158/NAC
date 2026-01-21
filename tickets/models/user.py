3b797adfeaa0320bd3ee7937e9b0d42045bc5c55
Akshaye Sharma <akshaye.bhatt@gmail.com>
2026-01-21 12:23:40 +0000

    Update user model

--- tickets/models/user.py
+++ tickets/models/user.py
@@ -1,12 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    first_name = models.VarcharField(max_length=30, not_null=True)
-    last_name = models.VarcharField(max_length=30, not_null=True)
-    email = models.VarcharField(max_length=30, not_null=True)
-    # profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+    """Model used for user authentication."""
+
+    # changed default unique=False to True, default blank=True to False
+    email = models.EmailField(unique=True, blank=False)
+    # changed default blank=True to False
+    first_name = models.CharField(max_length=50, blank=False)
+    # changed default blank=True to False
+    last_name = models.CharField(max_length=50, blank=False)
+
+    bio = models.CharField(max_length=500, blank=True)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def full_name(self):
+        """Return a string containing the user’s full name."""
+        return f"{self.first_name} {self.last_name}"
