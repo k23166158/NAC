36189af5e3ee16986e1cd3fbf9226aa4d3cea355
Akshaye Sharma <akshaye.bhatt@gmail.com>
2026-01-25 14:41:52 +0000

    Add ticket message thread view

---
+++ tickets/views/ticket_thread.py
@@ -0,0 +1,16 @@
+from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from ..models import Ticket, TicketMessage
+
+class TicketThreadView(LoginRequiredMixin, DetailView):
+    """View to display the thread of messages of tickets."""
+
+    model = Ticket
+    template_name = 'tickets/ticket_thread.html'
+    context_object_name = 'ticket'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['messages'] = TicketMessage.objects.filter(ticket=self.object)
+        return context
+    
