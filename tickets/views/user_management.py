8408f2d39f1bb11baee2406e458ac222670624c4
AshwinRajSingh <ashwin@aarnuk.com>
2026-02-02 16:27:06 +0000

    documentation updated

--- tickets/views/user_management.py
+++ tickets/views/user_management.py
@@ -1,25 +1,34 @@
-from django.views.generic import ListView
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Count, Q
 from django.shortcuts import get_object_or_404, redirect
 from django.views import View
+from django.views.generic import ListView
 
 User = get_user_model()
 
+
 class UserManagementView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    """View to list all users with pagination and search functionality."""
+
     model = User
     template_name = 'user_management.html'
     context_object_name = 'users'
     paginate_by = 10
 
     def test_func(self):
+        """Check if user is staff or superuser."""
         return self.request.user.is_staff or self.request.user.is_superuser
 
     def get_queryset(self):
+        """Return filtered and sorted user list."""
         queryset = User.objects.annotate(department_count=Count('user'))
+        return self._filter_and_order(queryset)
 
+    def _filter_and_order(self, queryset):
+        """Helper to filter and order queryset."""
         search_query = self.request.GET.get('q')
+
         if search_query:
             queryset = queryset.filter(
                 Q(username__icontains=search_query) |
@@ -29,22 +38,25 @@ class UserManagementView(LoginRequiredMixin, UserPassesTestMixin, ListView):
             )
 
         return queryset.order_by(
-            '-is_superuser',
-            '-is_staff',
-            'last_name',
-            'first_name'
+            '-is_superuser', '-is_staff', 'last_name', 'first_name'
         )
 
     def get_context_data(self, **kwargs):
+        """Add search query to context."""
         context = super().get_context_data(**kwargs)
         context['search_query'] = self.request.GET.get('q', '')
         return context
 
+
 class ToggleUserStatusView(LoginRequiredMixin, UserPassesTestMixin, View):
+    """View to toggle user activation status."""
+
     def test_func(self):
+        """Check if user is staff or superuser."""
         return self.request.user.is_staff or self.request.user.is_superuser
 
     def post(self, request, pk):
+        """Toggle the active status of the user."""
         user_to_toggle = get_object_or_404(User, pk=pk)
 
         # Prevent deactivating yourself
