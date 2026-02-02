f244e52595438a9eba77dc362ef803704c8a5bb9
ChathukaM <chathukapm@gmail.com>
2026-02-02 16:51:54 +0000

    Add staff-only DepartmentManageView

--- tickets/views/department_manage_view.py
+++ tickets/views/department_manage_view.py
@@ -1,10 +1,16 @@
 from django.views import View
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 
 
-class DepartmentManageView(LoginRequiredMixin, View):
-    """View for managing departments."""
+class DepartmentManageView(LoginRequiredMixin, UserPassesTestMixin, View):
+    """View for managing departments. Only accessible to staff members."""
+    login_url = '/login/'
+    raise_exception = False
+
+    def test_func(self):
+        """Check if the user is a staff member."""
+        return self.request.user.is_staff
 
     def get(self, request):
         """Handle GET requests for the department manage view."""
