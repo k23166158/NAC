7bc60b8f65eb1b5396dac79b85b7217fd69196d5
Ansh Thukral <thukral.ansh@gmail.com>
2026-02-03 20:08:20 +0000

    initial version of edit profile page

---
+++ tickets/views/profile_edit_view.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.views import redirect_to_login
+from django.shortcuts import redirect, render
+from django.views import View
+from django.db import IntegrityError
+
+
+class ProfileEditView(View):
+    """Allows the logged-in user to edit their profile."""
+
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return redirect_to_login(request.get_full_path())
+
+        return render(request, "profile_edit.html", {"user": request.user})
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return redirect_to_login(request.get_full_path())
+
+        user = request.user
+        user.first_name = request.POST.get("first_name", "").strip()
+        user.last_name = request.POST.get("last_name", "").strip()
+        user.username = request.POST.get("username", "").strip()
+        user.email = request.POST.get("email", "").lower().strip()
+
+        if "profile_picture" in request.FILES:
+            user.profile_picture = request.FILES["profile_picture"]
+
+        try:
+            user.save()
+        except IntegrityError:
+            return render(
+                request,
+                "profile_edit.html",
+                {
+                    "user": user,
+                    "error": "Email or username already exists.",
+                },
+            )
+
+        return redirect("my_profile")
