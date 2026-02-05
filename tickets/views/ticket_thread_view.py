7de9e05f5bde06f5c47a6556a94ed19bca76748b
Akshaye Sharma <akshaye.bhatt@gmail.com>
2026-02-05 17:22:01 +0000

    Update UI for forwarding tickets

--- tickets/views/ticket_thread_view.py
+++ tickets/views/ticket_thread_view.py
@@ -1,8 +1,16 @@
 from django.views.generic import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 from django.utils import timezone
+from requests import request
+from tickets.helpers.ticket_assignment import assign_staff_to_ticket
+
+
+from tickets.models.ticket_participant import TicketParticipant
 from ..models import Ticket, TicketMessage
+
+User = get_user_model()
 
 class TicketThreadView(LoginRequiredMixin, DetailView):
     """View to display the thread of messages of tickets."""
@@ -28,11 +36,26 @@ class TicketThreadView(LoginRequiredMixin, DetailView):
         """Get the ID of the last visible user message."""
         last_user_message = messages.filter(sender=self.request.user, hidden=False).last()
         return last_user_message.id if last_user_message else None
+    
+    def get_ticket_staff(self):
+        return [
+            p.user for p in self.object.participants.select_related("user")
+        ]
+
+    def get_available_staff(self, current_staff):
+        current_ids = [u.id for u in current_staff]
+        return User.objects.filter(is_staff=True).exclude(id__in=current_ids)
+
 
     def get_context_data(self, **kwargs):
         """Add ticket messages to the context."""
         context = super().get_context_data(**kwargs)
         messages = self.get_messages_queryset()
+
+        current_staff = self.get_ticket_staff()
+
+        context["staff"] = current_staff
+        context["available_staff"] = self.get_available_staff(current_staff)
 
         context["first_message"] = self.get_first_message(messages)
         context["messages"] = self.get_reply_messages(messages)
@@ -100,6 +123,38 @@ class TicketThreadView(LoginRequiredMixin, DetailView):
             )
             self.touch_ticket()
 
+    def handle_staff_change(self, request):
+        user_id = request.POST.get("user_id")
+        action = request.POST.get("action")
+
+        if not user_id:
+            return
+
+        user = get_object_or_404(User, id=user_id, is_staff=True)
+
+        if action == "add":
+            assign_staff_to_ticket(
+                ticket=self.object,
+                staff_user=user,
+                added_by=request.user,
+            )
+
+        elif action == "remove":
+            TicketParticipant.objects.filter(
+                ticket=self.object,
+                user=user,
+            ).delete()
+
+            TicketMessage.objects.create(
+                ticket=self.object,
+                sender=None,
+                body=f"{user.get_full_name()} was removed from the ticket.",
+            )
+
+        self.touch_ticket()
+
+
+
     def dispatch_post_action(self, action, request):
         """Dispatch POST action to the appropriate handler."""
         handlers = {
@@ -114,6 +169,10 @@ class TicketThreadView(LoginRequiredMixin, DetailView):
         """Handle POST actions for the ticket thread."""
         self.object = self.get_object()
         action = request.POST.get("action")
+
+        if action in {"add", "remove"}:
+            self.handle_staff_change(request)
+            return self.get(request, *args, **kwargs)
 
         self.dispatch_post_action(action, request)
         return self.get(request, *args, **kwargs)
