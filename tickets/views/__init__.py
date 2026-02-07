6f708866978dfe94be37d897bb710c230724cf01
nabeeliqbal1 <thedisposablemail01@gmail.com>
2026-02-07 17:57:56 +0000

    Add to init file

--- tickets/views/__init__.py
+++ tickets/views/__init__.py
@@ -1,4 +1,5 @@
 from .home_view import HomeView
+from .unauthenticated_home_view import UnauthenticatedHomeView
 from .ticket_thread_view import TicketThreadView
 from .auth import CustomLoginView, SignUpView
 from .department_view import DepartmentView
