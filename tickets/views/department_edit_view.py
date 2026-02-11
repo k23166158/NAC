d62a81895612b6fed4fbddb3b6ef5e098bcf3e47
k23166158 <k23166158>
2026-02-11 10:30:11 +0000

    prevent non-owners editing/deleting department

--- tickets/views/department_edit_view.py
+++ tickets/views/department_edit_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from tickets.views import DepartmentFormView
 from tickets.models import Department
+from django.http import HttpResponseForbidden
 
 class EditDepartmentView(DepartmentFormView):
     """View for editing an existing department. Only accessible to staff members."""
@@ -8,13 +9,25 @@ class EditDepartmentView(DepartmentFormView):
     def get(self, request, department_slug):
         """Handle GET requests - display the department form with existing data."""
         department = get_object_or_404(Department, slug=department_slug)
+
+        if department.created_by != request.user and not request.user.is_superuser:
+            return HttpResponseForbidden("You are not allowed to delete this department.")
+        
         return super().get(request, instance=department)
     
     def post(self, request, department_slug):
         """Handle POST requests - process the department form with existing data."""
         department = get_object_or_404(Department, slug=department_slug)
+
+        if department.created_by != request.user and not request.user.is_superuser:
+            return HttpResponseForbidden("You are not allowed to delete this department.")
+
         return super().post(request, instance=department)
 
     def render_form(self, request, form):
         """Helper method to render the form template."""
+
+        if form.instance.created_by != request.user and not request.user.is_superuser:
+            return HttpResponseForbidden("You are not allowed to delete this department.")
+
         return render(request, 'department_form.html', {'form': form, 'type': 'edit', 'department': form.instance})
