a967f37b260a30e355d6ffac0bcd1943313c30c1
Akshaye Sharma <akshaye.bhatt@gmail.com>
2026-03-07 12:14:08 +0000

    Add display of notifications

--- tickets/views/notifications_view.py
+++ tickets/views/notifications_view.py
@@ -2,11 +2,25 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
+from tickets.models.notification import Notification
+
 
 class NotificationView(LoginRequiredMixin, View):
     """View to display user notifications. Requires the user to be logged in."""
+
     template_name = "notifications.html"
 
     def get(self, request):
         """Handle GET requests to display the notifications page."""
-        return render(request, self.template_name)
+        notifications = (
+            Notification.objects.filter(actor=request.user)
+            .select_related("actor", "user")
+        )
+
+        return render(
+            request,
+            self.template_name,
+            {
+                "notifications": notifications,
+            },
+        )
