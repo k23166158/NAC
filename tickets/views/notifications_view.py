3c86424fdeca2bf8831e770883bc614773fc84b0
Akshaye Sharma <akshaye.bhatt@gmail.com>
2026-03-07 14:21:35 +0000

    Fix view for notification querying

--- tickets/views/notifications_view.py
+++ tickets/views/notifications_view.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
@@ -12,15 +13,11 @@ class NotificationView(LoginRequiredMixin, View):
 
     def get(self, request):
         """Handle GET requests to display the notifications page."""
-        qs = Notification.objects.filter(actor=request.user).select_related(
-            "actor", "user"
-        )
+        qs = (Notification.objects.filter(user=request.user).select_related("actor", "user").order_by("is_read", "-created_at"))
         notifications = list(qs)
-
-        Notification.objects.filter(actor=request.user, is_read=False).update(
+        Notification.objects.filter(user=request.user, is_read=False).update(
             is_read=True
         )
-
         return render(
             request,
             self.template_name,
