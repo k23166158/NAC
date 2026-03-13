8c146e1ba6f1d48be8ac5235a6bde3a5383a5d88
ChathukaM <chathukapm@gmail.com>
2026-03-13 15:26:09 +0000

    add new notification types

--- tickets/models/notification.py
+++ tickets/models/notification.py
@@ -9,6 +9,13 @@ class Notification(models.Model):
     class NotificationType(models.TextChoices):
         """Defines the types of notifications that can be created."""
         TICKET_CREATED = 'TICKET_CREATED', 'Ticket Created'
+        TICKET_CLOSED = 'TICKET_CLOSED', 'Ticket Closed'
+        STAFF_ASSIGNED = 'STAFF_ASSIGNED', 'Staff Assigned'
+        STAFF_REMOVED = 'STAFF_REMOVED', 'Staff Removed'
+        DEPT_ASSIGNED = 'DEPT_ASSIGNED', 'Department Assigned'
+        DEPT_REMOVED = 'DEPT_REMOVED', 'Department Removed'
+        NEW_MESSAGE = 'NEW_MESSAGE', 'New Message'
+        TICKET_FORWARDED = 'TICKET_FORWARDED', 'Ticket Forwarded'
     
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -29,7 +36,7 @@ class Notification(models.Model):
     target_object = GenericForeignKey('content_type', 'object_id')
 
     notification_type = models.CharField(
-        max_length=20,
+        max_length=30,
         choices=NotificationType.choices
     )
     
