643ee4fdecce80c3c4b188e56d8b313176975a0b
ChathukaM <chathukapm@gmail.com>
2026-03-19 13:29:49 +0000

    add department FAQ form

--- tickets/forms/__init__.py
+++ tickets/forms/__init__.py
@@ -1,3 +1,4 @@
 from .department_form import DepartmentForm
+from .department_faq_form import DepartmentFAQForm
 from .signup import SignUpForm
 from .forward_ticket import ForwardTicketForm
---
+++ tickets/forms/department_faq_form.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from ..models import DepartmentFAQ
+
+
+class DepartmentFAQForm(forms.ModelForm):
+    """Form for creating a department FAQ entry."""
+
+    class Meta:
+        model = DepartmentFAQ
+        fields = ['question', 'answer']
+        widgets = {
+            'question': forms.TextInput(attrs={
+                'class': 'department-form-input',
+                'placeholder': 'Enter your question',
+                'required': True,
+            }),
+            'answer': forms.Textarea(attrs={
+                'class': 'department-form-textarea',
+                'placeholder': 'Enter the answer',
+                'rows': 4,
+            }),
+        }
+        labels = {
+            'question': 'Question',
+            'answer': 'Answer',
+        }
